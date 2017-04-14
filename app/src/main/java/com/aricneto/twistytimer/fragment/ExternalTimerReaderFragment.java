8a6a4475d79a5b35a1d210089be631ca988476f5
Adrian Freund <freundTech@gmail.com>
2017-04-14 13:24:12 +0200

    Made packet parsing work below API level 24

--- app/src/main/java/com/aricneto/twistytimer/fragment/ExternalTimerReaderFragment.java
+++ app/src/main/java/com/aricneto/twistytimer/fragment/ExternalTimerReaderFragment.java
@@ -77,7 +77,9 @@ public class ExternalTimerReaderFragment extends Fragment {
 
         private static final int SAMPLE_RATE = 44100;
         private static final int BITRATE = 1200;
-        private static final float BITLENGTH = 44100/1200;
+        private static final float BITLENGTH = (float) SAMPLE_RATE/BITRATE;
+        private static final int bufferLength = SAMPLE_RATE / 8;
+        private static final short peekThreshold = 28000;
 
         public AudioRecorderThread() {
         }
@@ -87,18 +89,18 @@ public class ExternalTimerReaderFragment extends Fragment {
             paused = false;
 
             android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
-            int bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_8BIT);
+            //Using 16bit instead of 8 bit because 8 bit is unsigned and would require us doing a 2s complement
+            int bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT);
 
-            int bufferLength = SAMPLE_RATE / 8;
-
-            byte[] audioBuffer = new byte[bufferLength];
             //Double length in case packets are split between buffers
-            boolean[] dataBuffer = new boolean[bufferLength * 2];
+            short[] audioBuffer = new short[bufferLength * 2];
 
-            AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.UNPROCESSED,
+            final int audioSource = MediaRecorder.AudioSource.DEFAULT;
+
+            AudioRecord recorder = new AudioRecord(audioSource,
                     SAMPLE_RATE,
                     AudioFormat.CHANNEL_IN_MONO,
-                    AudioFormat.ENCODING_PCM_8BIT,
+                    AudioFormat.ENCODING_PCM_16BIT,
                     bufferSize);
 
             recorder.startRecording();
@@ -107,61 +109,67 @@ public class ExternalTimerReaderFragment extends Fragment {
             do {
                 result = recorder.read(audioBuffer, 0, bufferLength);
             }
-            while (result == 0);
+            while (result <= 0);
 
-            for (int i = 0; i < audioBuffer.length; i++) {
-                dataBuffer[i] = audioBuffer[i] < 0 ? false : true;
-            }
+            short threshold = 29000;
 
             while (running) {
-                result = recorder.read(audioBuffer, 0, bufferLength);
-                if (result == 0) {
+                result = recorder.read(audioBuffer, bufferLength, bufferLength);
+                if (result <= 0) {
                     try {
                         sleep(100);
                         continue;
                     }
                     catch (Exception e) {}
                 }
-                for (int i = 0; i < audioBuffer.length; i++) {
-                    dataBuffer[bufferLength + i] = audioBuffer[i] < 0 ? false : true;
-                }
 
-                int onecount = 0;
-                for (int i = 0; i < audioBuffer.length; i++) {
-                    if (dataBuffer[i]) {
-                        onecount++;
-                    } else {
-                        //Found packet start
-                        if (onecount >= 9 * BITLENGTH) {
-                            int limit = (int) (i + 90.5 * BITLENGTH); //8 bytes with 2 stop bits each
-                            byte[] packet = new byte[8];
+                findPackets(audioBuffer, threshold);
 
-                            ArrayList<Run> runs = runLengthEncode(dataBuffer, i, limit);
-                            ArrayList<Boolean> bits = getBitsFromRuns(runs);
-                            byte[] bytes = getBytesfromBits(bits);
-
-                            if (isValidPacket(bytes)) {
-                                TimerState state = new TimerState(bytes);
-                                if (!paused) {
-                                    handler.obtainMessage(TIMER_PACKET_WHAT, state).sendToTarget();
-                                }
-                            }
-
-                        }
-                        onecount = 0;
-
-                    }
-
-                }
-
-                //Shift data buffer
-                for (int i = bufferLength; i < dataBuffer.length; i++) {
-                    dataBuffer[i - bufferLength] = dataBuffer[i];
+                //Shift audio buffer
+                for (int i = bufferLength; i < audioBuffer.length; i++) {
+                    audioBuffer[i - bufferLength] = audioBuffer[i];
                 }
             }
             recorder.stop();
         }
 
+        private boolean findPackets(short[] audioBuffer, short threshold) {
+            boolean foundpacket = false;
+            int zerocount = 0;
+
+            for (int i = 0; i < audioBuffer.length-2; i++) {
+                if (audioBuffer[i] < peekThreshold && audioBuffer[i] > -peekThreshold) { //No peek
+                    zerocount++;
+                } else {
+                    //Found packet start
+                    if (zerocount >= 9 * BITLENGTH) {
+                        int limit = (int) (i + 90.5 * BITLENGTH); //8 bytes with 2 stop bits each. .5 for tolerance
+                        if (limit < audioBuffer.length) {
+                            ArrayList<Run> runs = runLengthEncodePeeks(audioBuffer, i, limit);
+                            ArrayList<Boolean> bits = getBitsFromRuns(runs);
+                            byte[] bytes = getBytesfromBits(bits);
+
+                            if (isValidPacket(bytes)) {
+                                foundpacket = true;
+                                TimerState state = new TimerState(bytes);
+                                if (!paused) {
+                                    handler.obtainMessage(TIMER_PACKET_WHAT, state).sendToTarget();
+                                }
+                            }
+                        }
+                    }
+                    zerocount = 0;
+                }
+            }
+
+            return foundpacket;
+        }
+
+        /**
+         * Checks if a packet is valid
+         * @param bytes packet to check
+         * @return if the packet is valid
+         */
         private boolean isValidPacket(byte[] bytes) {
             boolean isValid = " ACILRS".contains(String.valueOf((char) bytes[0])); //Timer State
 
@@ -179,25 +187,40 @@ public class ExternalTimerReaderFragment extends Fragment {
             return isValid;
         }
 
+        /**
+         * Turns an ArrayList of bits into an array of 9 bytes.
+         * @param bits Bits to convert
+         * @return array of 9 bytes or empty byte array if input isn't valid
+         */
         private byte[] getBytesfromBits(ArrayList<Boolean> bits) {
-            try {
+            if (bits.size() >= 90) {
                 byte[] bytes = new byte[9];
                 for (int i = 0; i < bytes.length; i++) {
-                    //One byte is one startbit, 8 data bits and one stop bit
-                    bytes[i] = 0;
-                    for (int j = 0; j <= 8; j++) {
-                        if (bits.get(i * 10 + j + 1)) {
-                            bytes[i] |= 0x01 << j;
+                    //One startbit, 8 data bits and one stop bit
+                    if (bits.get(i*10) == false && bits.get(i*10+9) == true) {
+                        bytes[i] = 0;
+                        for (int j = 0; j <= 8; j++) {
+                            if (bits.get(i * 10 + j + 1)) {
+                                bytes[i] |= 0x01 << j; //Most significant bit last
+                            }
                         }
+                    }
+                    else {
+                        return new byte[9];
                     }
                 }
                 return bytes;
             }
-            catch (Exception e) {
+            else {
                 return new byte[9];
             }
         }
 
+        /**
+         * Turn runs into an ArrayList of bits
+         * @param runs Runs to convert
+         * @return ArrayList of bits
+         */
         private ArrayList<Boolean> getBitsFromRuns(ArrayList<Run> runs) {
             ArrayList<Boolean> bits = new ArrayList<Boolean>();
             for(int i = 0; i < runs.size(); i++) {
@@ -209,25 +232,88 @@ public class ExternalTimerReaderFragment extends Fragment {
             return bits;
         }
 
-        private ArrayList<Run> runLengthEncode(boolean[] array, int start, int limit) {
-            ArrayList<Run> runs = new ArrayList<Run>();
-            int fulllength = 0;
-            int count = 0;
-            boolean lastvalue = array[start];
+        /**
+         * Returns run lengths from audioBuffer when AudioSource.UNPROCESSED is used.
+         * Currently unused, as AudioSource.UNPROCESSED is unly supported by API level 24 and newer.
+         *
+         * @param audioBuffer audioBuffer as returned by a AudioRecord with audiosource set to UNPROCESSED
+         * @param start Start index of the packet
+         * @param limit end index of the packet
+         * @return An ArrayList of Runs
+         */
+        @SuppressWarnings("unused")
+        private ArrayList<Run> runLengthEncode(short[] audioBuffer, int start, int limit) {
+            boolean dataBuffer[] = new boolean[limit-start];
             for (int i = start; i < limit; i++) {
-                if (array[i] == lastvalue) {
+                dataBuffer[i-start] = audioBuffer[i] < 0;
+            }
+
+            ArrayList<Run> runs = new ArrayList<Run>();
+            int count = 0;
+            boolean lastvalue = dataBuffer[0];
+            for (int i = 0; i < dataBuffer.length; i++) {
+                if (dataBuffer[i] == lastvalue) {
                     count++;
                 } else {
                     runs.add(new Run(lastvalue, count));
-                    fulllength += count;
-                    lastvalue = array[i];
+                    lastvalue = dataBuffer[i];
                     count = 0;
                 }
             }
             if (count != 0) {
                 runs.add(new Run(lastvalue, count));
+            }
+            return runs;
+        }
 
-                fulllength += count;
+        /**
+         * Returns run lengths from audioBuffer when AudioSource.DEFAULT is used.
+         *
+         * @param audioBuffer audioBuffer as returned by a AudioRecord with audiosource set to DEFAULT
+         * @param start Start index of the packet
+         * @param limit end index of the packet
+         * @return
+         */
+        private ArrayList<Run> runLengthEncodePeeks(short[] audioBuffer, int start, int limit) {
+            ArrayList<Run> runs = new ArrayList<Run>();
+            int count = 0;
+            int fulllength = 0;
+            boolean lastvalue = audioBuffer[start] < 0;
+
+            for (int i = start; i < limit-2; i++) {
+                count++;
+                if (lastvalue) {
+                    if (audioBuffer[i] > peekThreshold) {
+                        if(audioBuffer[i-5] > 25000) {
+                            continue;
+                        }
+                        while (audioBuffer[i + 1] >= audioBuffer[i] && i < limit - 2) {
+                            i++;
+                            count++;
+                        }
+                        runs.add(new Run(lastvalue, count));
+                        lastvalue = audioBuffer[i] < 0;
+                        count = 0;
+                    }
+                }
+                else {
+                    if (audioBuffer[i] < -peekThreshold) {
+                        if(audioBuffer[i-5] < -25000) {
+                            continue;
+                        }
+                        while (audioBuffer[i + 1] <= audioBuffer[i] && i < limit - 2) {
+                            i++;
+                            count++;
+                        }
+                        runs.add(new Run(lastvalue, count));
+                        lastvalue = audioBuffer[i] < 0;
+                        count = 0;
+                    }
+                }
+            }
+
+            if (count != 0) {
+                runs.add(new Run(lastvalue, count));
             }
             return runs;
         }
